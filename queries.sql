31bb0c0c40296684f677e149e922c1fa552e97a0
unknown <abodyalex1@gmail.com>
2022-08-09 23:04:36 +0300

    Add table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animal WHERE name LIKE '%mon';
+SELECT name FROM animal where date_of_birth BETWEEN 'Jan 01 2016' AND 'Jan 01 2019';
+SELECT * FROM animal WHERE neutered=true AND escape_attempts < 3;
+SELECT date_of_birth FROM animal WHERE name='Pikachu' OR name='Agumon';
+SELECT name, escape_attempts FROM animal WHERE weight_kg >10.5;
+SELECT * FROM animal WHERE neutered=true;
+SELECT * FROM animal WHERE name != 'Gabumon';
+SELECT * FROM animal WHERE weight_kg BETWEEN 10.4 AND 17.3;
