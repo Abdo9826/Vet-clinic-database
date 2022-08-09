60a741c6f98715098dbb736b892f831d0531dc71
unknown <abodyalex1@gmail.com>
2022-08-10 00:34:07 +0300

    Add changes

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 
 SELECT * FROM animal WHERE name LIKE '%mon';
-SELECT name FROM animal where date_of_birth BETWEEN 'Jan 01 2016' AND 'Jan 01 2019';
+SELECT name FROM animal where date_of_birth BETWEEN 'Jan 01 2016' AND ' Dec 31 2019';
 SELECT * FROM animal WHERE neutered=true AND escape_attempts < 3;
 SELECT date_of_birth FROM animal WHERE name='Pikachu' OR name='Agumon';
 SELECT name, escape_attempts FROM animal WHERE weight_kg >10.5;
